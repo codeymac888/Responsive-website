4bfdcdcc888558cc9576ecad41f7da15243ed9c6
codeymac888 <marquez.raj@gmail.com>
2017-09-18 19:56:12 +0400

    Final mobile responsive

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  //Hamburer menu toggle
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+});
